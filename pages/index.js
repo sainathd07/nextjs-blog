dcb373994d2dedfd7be138930de8c6c0224d317e
sainathd07 <saisainath777@gmail.com>
2024-04-20 20:15:35 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sainath Dushatti</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+       <h1>Sainath Dushatti</h1>
+       <h3>Welcome to my blog, or whatever!</h3>
+      </main>
+    </div>
+  )
+}
+
+      
