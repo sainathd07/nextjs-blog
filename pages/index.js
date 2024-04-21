787eace7762bb2d3b3006e1991ed3549c887abea
sainathd07 <saisainath777@gmail.com>
2024-04-21 08:39:08 -0400

    made changes to display blog posts on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import Head from 'next/head';
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+import utilStyles from '../styles/utils.module.css'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <div>
       <Head>
@@ -10,8 +22,22 @@ export default function Home() {
        <h1>Sainath Dushatti</h1>
        <h3>Welcome to my blog, or whatever!</h3>
       </main>
-    </div>
-  )
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+      </div>
+  );
 }
 
       
